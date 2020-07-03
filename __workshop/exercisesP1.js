9ddf5851eeac615416b4566fa140e1890ae879c6
Marie Foxall <marie@wastedeffort.ca>
2020-07-03 11:29:01 -0400

    Question 2

--- __workshop/exercisesP1.js
+++ __workshop/exercisesP1.js
@@ -6,7 +6,7 @@
 const q1 = (req, res) => {
   // there is nothing to do here for this question
 
-  res.render('pages/question1');
+  res.render("pages/question1");
 };
 
 // -----------------------------------------------
@@ -14,9 +14,11 @@ const q1 = (req, res) => {
 // -----------------------------------------------
 const q2 = (req, res) => {
   // pass this variable to the ejs file and render it in the answer box.
-  const sentence = '<p>This is the way.</p>';
+  const sentence = "<p>This is the way.</p>";
 
-  res.render('pages/question2', {});
+  res.render("pages/question2", {
+    sentence: sentence,
+  });
 };
 
 // -----------------------------------------------
@@ -26,33 +28,33 @@ const q3 = (req, res) => {
   // pass this information to ejs file and render it in the answer box.
   const homer = {
     imageUrl:
-      'https://vignette.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png',
-    name: 'Homer Jay Simpson',
-    dob: 'May 12, 1956',
-    profession: 'Low-level Safety Inspector',
-    favoriteFood: 'Donuts',
-    favoriteBeverage: 'Duff Beer',
+      "https://vignette.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png",
+    name: "Homer Jay Simpson",
+    dob: "May 12, 1956",
+    profession: "Low-level Safety Inspector",
+    favoriteFood: "Donuts",
+    favoriteBeverage: "Duff Beer",
   };
 
-  res.render('pages/question3');
+  res.render("pages/question3");
 };
 
 // -----------------------------------------------
 // Question 4
 // -----------------------------------------------
 const q4 = (req, res) => {
-  const popularGirlNames = ['Olivia', 'Ruby', 'Emily', 'Grace', 'Jessica'];
+  const popularGirlNames = ["Olivia", "Ruby", "Emily", "Grace", "Jessica"];
 
-  res.render('pages/question4');
+  res.render("pages/question4");
 };
 
 // -----------------------------------------------
 // Question 5
 // -----------------------------------------------
 const q5 = (req, res) => {
-  const popularGirlNames = ['Olivia', 'Ruby', 'Emily', 'Grace', 'Jessica'];
+  const popularGirlNames = ["Olivia", "Ruby", "Emily", "Grace", "Jessica"];
 
-  res.render('pages/question5');
+  res.render("pages/question5");
 };
 
 // We export the functions so that they may be 'required' or imported in other files.
